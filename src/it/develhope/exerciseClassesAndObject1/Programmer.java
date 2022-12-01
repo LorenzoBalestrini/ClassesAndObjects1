5f2e5e5f23b16e445a56da0ab33fb3dd5328c35a
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-01 14:32:41 +0100

    Primo esercizio

--- src/it/develhope/exerciseClassesAndObject1/Programmer.java
+++ src/it/develhope/exerciseClassesAndObject1/Programmer.java
@@ -6,15 +6,15 @@ public class Programmer {
     public int age;
     public boolean wearGlasses;
 
-    public static void drinkCoffee (){
+    public void drinkCoffee (){
         System.out.println("Espresso is the secret!");
     }
 
-    public void printDetails (){
-        System.out.println(this.name + " is a " + this.age + "-yo programmer");
+    public String printDetails (){
+        return name + " is a " + age + "-yo programmer";
     }
 
-    public void hasGlasses(){
-        System.out.println("Is " + this.name + " wearing glasses? - " + wearGlasses);
+    public String hasGlasses(){
+        return "Is " + name + " wearing glasses? - " + wearGlasses;
     }
 }
